05f7497d77322d17a9eda63948af8c722cb76d8b
stefan62-19 <82461668+stefan62-19@users.noreply.github.com>
2021-04-14 11:51:27 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1227,7 +1227,8 @@ if(url.indexOf('checkout.html')!=-1)
 			$("#Payment").val("0");
 			$("#field-10").val("");
 			$("#checkbox-4").prop("checked", false);
-
+			$("#korpa").html("<h1>Cart is empty</h1><br><form action='products.html'><button style='margin:0px auto;'>Books</button></form>");
+			localStorage.removeItem('cartProducts');
 		}
 	})
 
