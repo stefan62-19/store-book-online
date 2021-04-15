d7ee0e46baba92cdcfd4de31d1c2c632f3d1803d
stefan62-19 <82461668+stefan62-19@users.noreply.github.com>
2021-04-15 16:05:09 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,7 +185,7 @@ $.ajax({
 			{
 				$("#menu #second").append('<li><a href="'+element.href+'" >'+element.naziv+'</a></li>')
 			}
-			if(element.naziv=='Contact Us'||element.naziv=='Author')
+			if(element.naziv=='Contact Us'||element.naziv=='Author'||element.naziv=='Documentation')
 			{
 				$("#menu #first").append('<li><a href="'+element.href+'" >'+element.naziv+'</a></li>')
 			}
